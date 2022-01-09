b7bcf0b058ddaf8b49803254b5a70c4702c4fb2b
Caleb <choover11@gmail.com>
2022-01-08 17:16:04 -0800

    added netlify badge

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Randomizer
 
+[![Netlify Status](https://api.netlify.com/api/v1/badges/507754a2-1e23-484e-9383-1a7b5944b3a9/deploy-status)](https://app.netlify.com/sites/simple-randomizer/deploys)
+
+
 Very simple website to provide a way of listing items and generating a random choice among them.
 
 Made with React and TailwindCSS
--- src/RandomizerPage.js
+++ src/RandomizerPage.js
@@ -50,7 +50,7 @@ function RandomizerPage() {
             value={options}
             onChange={(e) => setOptions(e.currentTarget.value)}
             className="resize-none	bg-blue-500 text-white h-full flex-1 border border-gray-300 rounded-md p-1 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-            autofocus
+            autoFocus
           />
           <p>
             Items: {items.length}
